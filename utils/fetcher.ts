dc7c8690c97540f4da326857c821c3a22eed884e
Hector Sosa <hectorsosa@Hectors-MBP.home>
2022-05-15 15:21:34 +0200

    Fixing fetcher

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-anonymous-default-export
-export default async function (...args: any) {
-	const res = await fetch(...args);
+export default async function (endpoint: any, config: any) {
+	const res = await fetch(endpoint, config);
 	return res.json();
 }
